8b5cce9f11a9e663b710cd3993c91a4b99cd3661
lvcz <lvcz@github.com>
2017-06-04 17:23:03 -0300

    versao0406

--- trabalho.py
+++ trabalho.py
@@ -1,29 +1,41 @@
 import socket
 import os
 import sys
-HOST = '127.0.0.1'              # Endereco IP do Servidor
+import pickle
+import time
+
+#HOST = '127.0.0.1'              # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-peers = []
+peers = list()
 
 
 def cliente(host):
     dest = (host, PORT)
-    tcp.connect(dest)    
+    tcp.connect(dest)
+    jpeers = tcp.recv(1024)
+    print jpeers
+    recpeers = pickle.loads(jpeers)
     while True:
-      #tcp.send (msg)
-      peers = tcp.recv(1024)      
+        #tcp.send (msg)
+        lpeers = tcp.recv(1024)
+        if lpeers != jpeers:
+            jpeers = lpeers
+            recpeers = pickle.loads(jpeers)
+        #print jpeers
+        
     tcp.close()
-    if peers[0] == HOST:
+    if recpeers[0] == HOST:
         servidor()
     else:
         cliente(msg[0])
 
-def servidor():
-    orig = (HOST, PORT)
+def servidor(host):
+    orig = (host, PORT)
     tcp.bind(orig)
     tcp.listen(1)
+    print 'Servidor'
     while True:
         con, cliente = tcp.accept()
         pid = os.fork()
@@ -31,8 +43,12 @@ def servidor():
             tcp.close()
             print 'Conectado por', cliente
             peers.append(cliente)
+            print peers
+            jpeers = pickle.dumps(peers)
+            print jpeers
             while True:
-                con.sendall(peers)
+                con.sendall(jpeers)
+                time.sleep(15)
                 #print cliente, msg
             print 'Finalizando conexao do cliente', cliente
             con.close()
@@ -41,8 +57,10 @@ def servidor():
             con.close()
 
 
-if len(sys.argv) == 1:
-    servidor()
-else:
-    cliente(sys.argv[1])
+if len(sys.argv) != 3:
+    print 'usage: python program.py <-s>(Servidor) <-c> (cliente) <IP>(meu ip se for -s) (ip Servidor)'
+elif sys.argv[1] =='-s':
+    servidor(sys.argv[2])
+elif sys.argv[1] == '-c':
+    cliente(sys.argv[2])
     
